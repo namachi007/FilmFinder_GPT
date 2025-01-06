fc905bf5f14a8460b326798e54e3aa47909e2e0a
Namasivayam M <iamnamachu40@gmail.com>
2025-01-06 15:03:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline text-center m-12">
+          Netflix GPT
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
